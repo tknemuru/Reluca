1e88bb7d2fa0432a4e157f4325d3a5f2446a8075
tknemuru <kojikoji.max.5252@gmail.com>
2024-02-04 01:40:37 +0900

    add FeaturePatternExtractor.

---
+++ Reluca/Models/FeaturePattern.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Reluca.Models
+{
+    /// <summary>
+    /// 特徴パターン
+    /// </summary>
+    public static class FeaturePattern
+    {
+        /// <summary>
+        /// 種類と名称の変換辞書
+        /// </summary>
+        private static readonly Dictionary<string, Type> TypeNameDic = new Dictionary<string, Type>()
+        {
+            [TypeName.Diag4] = Type.Diag4,
+            [TypeName.Diag5] = Type.Diag5,
+            [TypeName.Diag6] = Type.Diag6,
+            [TypeName.Diag7] = Type.Diag7,
+            [TypeName.Diag8] = Type.Diag8,
+            [TypeName.HorVert2] = Type.HorVert2,
+            [TypeName.HorVert3] = Type.HorVert3,
+            [TypeName.HorVert4] = Type.HorVert4,
+            [TypeName.Edge2X] = Type.Edge2X,
+            [TypeName.Corner2X5] = Type.Corner2X5,
+            [TypeName.Corner3X3] = Type.Corner3X3,
+        };
+
+        /// <summary>
+        /// 種類
+        /// </summary>
+        public enum Type
+        {
+            Diag4,
+            Diag5,
+            Diag6,
+            Diag7,
+            Diag8,
+            HorVert2,
+            HorVert3,
+            HorVert4,
+            Edge2X,
+            Corner2X5,
+            Corner3X3,
+        }
+
+        /// <summary>
+        /// 種類名称
+        /// </summary>
+        public static class TypeName
+        {
+            public const string Diag4 = "diag4";
+            public const string Diag5 = "diag5";
+            public const string Diag6 = "diag6";
+            public const string Diag7 = "diag7";
+            public const string Diag8 = "diag8";
+            public const string HorVert2 = "hor_vert2";
+            public const string HorVert3 = "hor_vert3";
+            public const string HorVert4 = "hor_vert4";
+            public const string Edge2X = "edge2X";
+            public const string Corner2X5 = "corner2X5";
+            public const string Corner3X3 = "corner3X3";
+        }
+
+        /// <summary>
+        /// 盤状態の連番
+        /// </summary>
+        public static class BoardStateSequence
+        {
+            /// <summary>
+            /// 白
+            /// </summary>
+            public const ulong White = 0ul;
+            /// <summary>
+            /// 空
+            /// </summary>
+            public const ulong Empty = 1ul;
+            /// <summary>
+            /// 黒
+            /// </summary>
+            public const ulong Black = 2ul;
+        }
+
+        /// <summary>
+        /// 名称から種類を取得します。
+        /// </summary>
+        /// <param name="name">種類名称</param>
+        /// <returns>種類</returns>
+        public static Type GetType(string name)
+        {
+            return TypeNameDic[name];
+        }
+    }
+}
--- Reluca/Services/FeaturePatternExtractor.cs
+++ Reluca/Services/FeaturePatternExtractor.cs
@@ -1,5 +1,6 @@
 ﻿using Reluca.Contexts;
 using Reluca.Helpers;
+using Reluca.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +10,75 @@ using System.Threading.Tasks;
 namespace Reluca.Services
 {
 #pragma warning disable CS8602 // null 参照の可能性があるものの逆参照です。
-    public class FeaturePatternExtractor : IServiceable<GameContext, Dictionary<string, List<List<ulong>>>>
+#pragma warning disable CS8604
+    public class FeaturePatternExtractor : IServiceable<GameContext, Dictionary<FeaturePattern.Type, List<ulong>>>
     {
-        public Dictionary<string, List<List<ulong>>> Execute(GameContext input)
+        /// <summary>
+        /// 特徴パターンの位置情報を管理する辞書
+        /// </summary>
+        public Dictionary<FeaturePattern.Type, List<List<ulong>>> PatternPositions { get; set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public FeaturePatternExtractor()
         {
-            var patterns = FileHelper.ReadJson<Dictionary<string, List<List<ulong>>>>(Properties.Resources.feature_pattern);
-            Console.WriteLine(patterns);
-            return patterns;
+            PatternPositions = ReadPatternPositions();
+        }
+
+        public Dictionary<FeaturePattern.Type, List<ulong>> Execute(GameContext context)
+        {
+            var result = new Dictionary<FeaturePattern.Type, List<ulong>>();
+            foreach (var pattern in PatternPositions)
+            {
+                result[pattern.Key] = new List<ulong>();
+                foreach (var positions in pattern.Value)
+                {
+                    result[pattern.Key].Add(ConvertToTernaryIndex(context.Board, positions));
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 特徴パターンの位置情報辞書を読み込みます。
+        /// </summary>
+        /// <returns>特徴パターンの位置情報辞書</returns>
+        private static Dictionary<FeaturePattern.Type, List<List<ulong>>> ReadPatternPositions()
+        {
+            var resource = FileHelper.ReadJson<Dictionary<string, List<List<ulong>>>>(Properties.Resources.feature_pattern);
+            // 文字列操作を避けるために、キーを文字列からenumに変換して保持する
+            var positions = resource.ToDictionary(r =>  FeaturePattern.GetType(r.Key), r => r.Value);
+            return positions;
+        }
+
+        /// <summary>
+        /// 盤状態を特徴パターンに従って3進数変換したインデックスに変換します。
+        /// </summary>
+        /// <param name="context">盤状態</param>
+        /// <param name="positions">特徴パターンの位置情報</param>
+        /// <returns>盤状態を特徴パターンに従って3進数変換したインデックス</returns>
+        private static ulong ConvertToTernaryIndex(BoardContext context, List<ulong> positions)
+        {
+            var index = 0ul;
+            var length = positions.Count;
+            for (var i = 0; i < length; i++)
+            {
+                var value = FeaturePattern.BoardStateSequence.Empty;
+                if ((context.White & positions[0]) > 0)
+                {
+                    value = FeaturePattern.BoardStateSequence.White;
+                } else if ((context.Black & positions[0]) > 0)
+                {
+                    value = FeaturePattern.BoardStateSequence.Black;
+                }
+                index += value;
+                if (i < length - 1)
+                {
+                    index *= 3;
+                }
+            }
+            return index;
         }
     }
 }
